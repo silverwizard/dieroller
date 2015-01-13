6976fd74dac87658fd2e4551f9dc98750ab9af8f
Silverwizard <silver@callysto.com>
2015-01-13 16:14:03 -0500

    Let's add an ID field so we can more effectively query!

--- roller.rb
+++ roller.rb
@@ -4,7 +4,7 @@ require "./die"
 require "cgi"
 cgi = CGI.new
 result = roll(cgi.params["rollstring"][0].to_s)
-statement = $my.prepare("INSERT INTO rolls (id, roll, result, user, game) VALUES (" + $my.insert_id.to_s + ", '" + cgi.params["rollstring"][0] + "', '" + result.to_s + "', '" + cgi.params["user"][0] + "', '" + cgi.params["game"][0] + "');")
+statement = $my.prepare("INSERT INTO rolls (roll, result, user, game) VALUES ('" + cgi.params["rollstring"][0] + "', '" + result.to_s + "', '" + cgi.params["user"][0] + "', '" + cgi.params["game"][0] + "');")
 statement.execute
 puts result
 $my.close
